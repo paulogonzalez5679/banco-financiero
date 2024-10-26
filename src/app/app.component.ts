fab8a9f83c29d2611be011507535ee27209690c6
paulogonzalez5679 <paulomga10@gmail.com>
2024-10-25 22:51:15 -0500

    Pruebas realizadas con Karma

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'banco-financiero';
+  title = 'Hello, banco-financiero'; // Asegúrate de que esta propiedad esté definida
 }
